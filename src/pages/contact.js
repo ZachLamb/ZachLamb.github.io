26c87e07ed75935a6693ebe44e79f2ddcc76fcae
zach.lamb <zach.lamb@regiscompany.com>
2020-02-20 18:48:09 -0500

    added in react and styling for contact and blog

---
+++ src/pages/contact.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { graphql } from "gatsby";
+
+import CardLayout from "../components/cardLayout";
+import Layout from "../components/mainLayout";
+import ProgressLayout from "../components/progressLayout";
+
+import Grow from "@material-ui/core/Grow";
+import {
+  Grid,
+  Card,
+  FormControl,
+  FormHelperText,
+  Typography,
+  Icon,
+  Input,
+  InputBase,
+  InputLabel,
+  IconButton,
+  Paper,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { FaLinkedinIn,FaGithub,FaDev, } from 'react-icons/fa';
+
+import "../styles/main.scss";
+
+export default ({ data }) => (
+  <Layout pageStyle={"contact-page"}>
+    <Grid container direction="column" alignItems="flex-start" justify="space-between">
+      <Grid item>
+        <h2>Contact Me</h2>
+        <h5>Connect with me on social media or send me your contact info</h5>
+        <Grid container className="social-media">
+         <IconButton ><a href="https://www.linkedin.com/in/lambzachary/" target="_blank"><FaLinkedinIn/></a></IconButton>
+         <IconButton ><a href="https://github.com/ZachLamb" target="_blank"><FaGithub/></a></IconButton>
+         <IconButton ><a href="https://dev.to/zachlamb" target="_blank"><FaDev/></a></IconButton>
+         
+        </Grid>
+      </Grid>
+      <Grid item>
+      <Paper className="contact-card">
+      <form
+        method="post" 
+        netlify-honeypot="bot-field" 
+        data-netlify="true" 
+        data-netlify-recaptcha="true"
+        >
+        <input type="hidden" name="bot-field" />
+        <TextField
+          id="outlined-secondary"
+          label="Email"
+          variant="outlined"
+          color="secondary"
+          name="email"
+          type="input"
+          autoFocus
+        />
+        <TextField
+          id="outlined-secondary"
+          label="Name"
+          variant="outlined"
+          color="secondary"
+          name="name"
+          type="input"
+        />
+        <TextField
+          id="outlined-secondary"
+          label="Message"
+          variant="outlined"
+          color="secondary"
+          name="message"
+          type="input"
+          fullWidth
+        />
+        <Button color="primary" type="submit">Send email to Zach</Button>
+      </form>
+      </Paper>
+      </Grid>
+    </Grid>
+  </Layout>
+);
