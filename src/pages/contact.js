93bef5860038fd3dfa0f36b878c72cbaffe18eea
zach.lamb <zach.lamb@regiscompany.com>
2020-02-21 13:00:15 -0800

    added name att to form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -42,6 +42,7 @@ export default ({ data }) => (
       <Grid item>
       <Paper className="contact-card">
       <form
+        name="contact"
         method="post" 
         netlify-honeypot="bot-field" 
         data-netlify="true" 
@@ -74,7 +75,7 @@ export default ({ data }) => (
           type="input"
           fullWidth
         />
-        <Button color="primary" type="submit">Send email to Zach</Button>
+        <Button color="primary" type="submit" action>Send email to Zach</Button>
       </form>
       </Paper>
       </Grid>
