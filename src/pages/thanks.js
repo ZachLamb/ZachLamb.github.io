76ff1c9e7135f3ee18e9b28fd9e60ff85309eec4
zach.lamb <zach.lamb@regiscompany.com>
2020-02-22 21:15:23 -0800

    added thank you page

---
+++ src/pages/thanks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Layout from '../layout'
+
+export default () => (
+  <Layout>
+    <h1>Thank you!</h1>
+    <p>I will get back to you as soon as I can :)</p>
+  </Layout>
+)
