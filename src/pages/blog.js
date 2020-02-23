15f35c8a514a3eea713d907ee7ab45a9a5197837
zach.lamb <zach.lamb@regiscompany.com>
2020-02-22 21:13:14 -0800

    removed unused imports and added in react post for contact form

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Helmet } from "react-helmet";
 import { graphql } from "gatsby";
 
 import CardLayout from "../components/cardLayout";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,5 @@
 import React from "react";
-import { graphql } from "gatsby";
-
-import CardLayout from "../components/cardLayout";
 import Layout from "../components/mainLayout";
-import ProgressLayout from "../components/progressLayout";
-import Grow from "@material-ui/core/Grow";
-import { Grid, Card, Typography } from "@material-ui/core";
 
 import "../styles/main.scss";
 import Logo from "../components/logo";
