0bf38009e00304491dd3d1891be230aa34b3e3cc
zach.lamb <zach.lamb@regiscompany.com>
2020-08-12 15:55:57 -0700

    added image to blog

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -13,6 +13,7 @@ const usePosts = () => {
               published_at(formatString: "MM-DD-YYYY")
               description
               body_markdown
+              cover_image
             }
           }
         }
@@ -26,6 +27,7 @@ const usePosts = () => {
         date: post.node.article.published_at,
         desc: post.node.article.description,
         body: post.node.article.body_markdown,
+        image: post.node.article.cover_image
     }))
 }
 
